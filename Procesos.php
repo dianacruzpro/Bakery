515564cfa6db4e24c213b017acccb94cb9e856df
Diana Cruz <dsarai1511@gmail.com>
2022-05-16 23:51:57 -0600

    agregue un modal mas

--- Procesos.php
+++ Procesos.php
@@ -20,7 +20,8 @@
         <h3>Pan Frances</h3>
       </div>
     </a>
-    <a class="tarjeta text-decoration-none" href="#receta-2" style="--i:url(../IMG/FRANCES-INTEGRAL.jpg)">
+    <a class="tarjeta text-decoration-none" href="#receta-2" style="--i:url(../IMG/FRANCES-INTEGRAL.jpg)"
+    data-bs-toggle="modal" data-bs-target="#receta-2">
       <div class="content">
         <h3>Pan integral</h3>
       </div>
@@ -60,6 +61,53 @@
       <div class="modal-body">
         <div class="portada-modal">
           <img src="./IMG/pan-frances.jpg" alt="receta 1">
+          <div class="curva" style="height: 150px; overflow: hidden;"><svg viewBox="0 0 500 150" preserveAspectRatio="none" style="height: 100%; width: 100%;">
+              <path d="M-20.03,18.27 C317.43,327.14 332.67,-122.84 513.26,117.94 L512.13,189.97 L0.00,150.00 Z" style="stroke: none; fill: #fff;"></path>
+            </svg></div>
+        </div>
+        <div class="receta-info">
+          <h4 class="my-4 text-center text-dark">Procedimiento</h4>
+          <div class="steps">
+            <div class="step pt-4">
+              <img src="./IMG/one.png" alt="">
+              <p class="text-dark">Pesar correctamente todos los ingredientes y colocar en la amasadora sal, azúcar, manteca, agua y por último la levadura. Mezclar por 2 minutos en primera velocidad</p>
+            </div>
+            <div class="step pt-4">
+              <img src="./IMG/two.png" alt="">
+              <p class="text-dark">Incorporar harina dura y amasar 8 minutos en segunda velocidad, luego cubrir la masa (manta o nylon) y dejar fermentar de 45 minutos a 1 hora</p>
+            </div>
+            <div class="step pt-4">
+              <img src="./IMG/three.png" alt="">
+              <p class="text-dark">Refinar la masa hasta desarrollar el gluten, bolear al peso deseado, luego colocar en bandejas y reposar de 15 a 20 minutos para figurar</p>
+            </div>
+            <div class="step pt-4">
+              <img src="./IMG/four.png" alt="">
+              <p class="text-dark">Ingresar el producto a cámara de fermentación de 45 a 60 minutos ( temperatura 36°C a 40°C y humedad relativa de 70% a 90%) o a temperatura ambiente hasta que duplique su tamaño</p>
+            </div>
+            <div class="step pt-4">
+              <img src="./IMG/five.png" alt="">
+              <p class="text-dark">Hornear a 180° C (356° F) de 18 a 20 minutos</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Hecho</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="modal fade" id="receta-2" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg modal-dialog-scrollable modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title text-dark" id="staticBackdropLabel">Pan Integral</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <div class="portada-modal">
+          <img src="./IMG/FRANCES-INTEGRAL.jpg" alt="receta 2">
           <div class="curva" style="height: 150px; overflow: hidden;"><svg viewBox="0 0 500 150" preserveAspectRatio="none" style="height: 100%; width: 100%;">
               <path d="M-20.03,18.27 C317.43,327.14 332.67,-122.84 513.26,117.94 L512.13,189.97 L0.00,150.00 Z" style="stroke: none; fill: #fff;"></path>
             </svg></div>
