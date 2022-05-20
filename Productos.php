3f6c37af2dd46d4916d1db3932598d62ea27bf71
Ronald Cornejo <55774242+Ronald151117@users.noreply.github.com>
2022-05-19 23:37:35 -0600

    Update Productos.php

--- Productos.php
+++ Productos.php
@@ -1,5 +1,33 @@
 <?php include("./TEMPLATE/cabecera.php") ?>
 
+<section class="productos_portada">
+  <div class="texto_portada">
+    <h1>Productos</h1>
+    <p>"Para los paladares mas exigentes".</p>
+  </div>
+</section>
+<div><h2 class="primero">Nuestras especialidades</h2></div>
+<div class="container">
+
+    <div class="pasteleria">
+    <a title="Pasteleria" href="http://localhost/Bakery/pasteleria.php"><img src="IMG/Tipos/Pasteleria/Pasteleria.jpg"  width="480" height="400"alt="Pastel" /><center><h1>Pasteleria</h1></center></a>
+    </div>
+
+    <div class="reposteria">
+    <a title="Reposteria" href="http://localhost/Bakery/reposteria.php"><img src="IMG/Tipos/Reposteria/Reposteria.jpg"  width="480" height="400" alt="Repos" /><center><h1>Reposteria</h1></center></a>
+    </div>
+
+    <div class="cafeteria" >
+    <a title="Cafeteria" href="http://localhost/Bakery/cafeteria.php"><img src="IMG/Tipos/Cafeteria/cafeteria.jpg" width="480" height="400"  alt="Cafe" /><center><h1>Cafeteria</h1></center></a>
+    </div>
+
+    <div class="panaderia" >
+    <a title="Panaderia" href="http://localhost/Bakery/panaderia.php"><img src="IMG/Tipos/Panaderia/Quesadillas.jpg" width="480" height="400"  alt="Pan" /><center><h1>Panaderia</h1></center></a>
+    </div>
+
+    
+</div>
+
 
 
 <?php include("./TEMPLATE/pie.php") ?>
