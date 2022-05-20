426ca6ed894e64f71eb37e1ae618fd1de8ee3320
Diana Cruz <dsarai1511@gmail.com>
2022-05-20 11:21:23 -0600

    modifique e hice responsive productos

--- Productos.php
+++ Productos.php
@@ -6,26 +6,31 @@
     <p>"Para los paladares mas exigentes".</p>
   </div>
 </section>
-<div><h2 class="primero">Nuestras especialidades</h2></div>
+<div class="text-especialidades-container"><h2 class="primero">Nuestras especialidades</h2></div>
 <div class="container">
 
-    <div class="pasteleria">
-    <a title="Pasteleria" href="http://localhost/Bakery/pasteleria.php"><img src="IMG/Tipos/Pasteleria/Pasteleria.jpg"  width="480" height="400"alt="Pastel" /><center><h1>Pasteleria</h1></center></a>
-    </div>
-
-    <div class="reposteria">
-    <a title="Reposteria" href="http://localhost/Bakery/reposteria.php"><img src="IMG/Tipos/Reposteria/Reposteria.jpg"  width="480" height="400" alt="Repos" /><center><h1>Reposteria</h1></center></a>
-    </div>
-
-    <div class="cafeteria" >
-    <a title="Cafeteria" href="http://localhost/Bakery/cafeteria.php"><img src="IMG/Tipos/Cafeteria/cafeteria.jpg" width="480" height="400"  alt="Cafe" /><center><h1>Cafeteria</h1></center></a>
-    </div>
-
-    <div class="panaderia" >
-    <a title="Panaderia" href="http://localhost/Bakery/panaderia.php"><img src="IMG/Tipos/Panaderia/Quesadillas.jpg" width="480" height="400"  alt="Pan" /><center><h1>Panaderia</h1></center></a>
-    </div>
-
-    
+  <div class="fila">
+    <a class="tarjeta text-decoration-none" href="pasteleria.php" style="--i:url(../IMG/Tipos/Pasteleria/Pasteleria.jpg)" alt="pastel">
+      <div class="content">
+        <h3>PASTELERIA</h3>
+      </div>
+    </a>
+    <a class="tarjeta text-decoration-none" href="reposteria.php" style="--i:url(../IMG/Tipos/Reposteria/Reposteria.jpg)" alt="pastel">
+      <div class="content">
+        <h3>REPOSTERIA</h3>
+      </div>
+    </a>
+    <a class="tarjeta text-decoration-none" href="cafeteria.php" style="--i:url(../IMG/Tipos/Cafeteria/cafeteria.jpg)" alt="pastel">
+      <div class="content">
+        <h3>CAFETERIA</h3>
+      </div>
+    </a>
+    <a class="tarjeta text-decoration-none" href="panaderia.php" style="--i:url(../IMG/Tipos/Panaderia/Quesadillas.jpg)" alt="pastel">
+      <div class="content">
+        <h3>PANADERIA</h3>
+      </div>
+    </a>
+  </div>
 </div>
 
 
