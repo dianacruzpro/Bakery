e30f9efc08a15e869fd8364fb648b6ac67537401
STEFANY HERNANDEZ~ <stephanie.aguilar56865@uped.edu.sv>
2022-05-20 19:11:20 -0600

    agregamos un parrafo

--- Contacto.php
+++ Contacto.php
@@ -1,5 +1,53 @@
 <?php include("./TEMPLATE/cabecera.php") ?>
 
 
+<body>
+    <div class="container">
+        <h1 class="text-left" style="color: #f3f3f3">Contactanos</h1>
+        <div class="row">
+            <div class="col-md-12">
+                <form method="post" action="enviarcorreo.php">
+                    <ul class="contact-form">
+                        <li>
+                            <div class="col-md-6">
+                                <input name="nombre" placeholder="Escribe tu nombre" required="required" size="8" type="text" />
+                            </div>
+
+                            <div class="col-md-6">
+                                <input name="email" placeholder="Email" required="required" size="8" type="email" />
+                            </div>
+                        </li>
+
+                        <li>
+                            <div class="col-md-6">
+                                <input name="telefono" placeholder="Escribe tu numero" required="required" size="8" type="text" />
+                            </div>
+
+                        </li>
+                        <li>
+                            <div class="col-md-12">
+                                <input name="asunto" placeholder="Asunto" required="required" size="8" type="text" />
+                            </div>
+                        </li>
+
+                        <li>
+                            <div class="col-md-12">
+                                <button type="submit">
+                    Enviar mensaje
+                    <span
+                      class="glyphicon glyphicon-arrow-right"
+                      aria-hidden="true"
+                    ></span>
+                  </button>
+                            </div>
+                        </li>
+                    </ul>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+<link rel="stylesheet" href="css/style.css" />
+</html>
 
 <?php include("./TEMPLATE/pie.php") ?>
