049631c791edfcb52fa301b004a9f53ca91c720c
Taikyko <taikyko.a@gmail.com>
2022-05-21 21:53:34 -0600

    Agregar apartado de acerca de

--- Acerca.php
+++ Acerca.php
@@ -1,5 +1,193 @@
 <?php include("./TEMPLATE/cabecera.php") ?>
 
+<section>
+    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <div class="banner_uno">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-6 texto_banner">
+                                <h1><span>•┃</span> NOSOTROS <span>┃•</span></h1>
+                                <hr>
+                                <h2> Panaderia, pasteleria y reposteria.</h2>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                                Minima impedit a aperiam officia vel quasi eveniet doloremque maiores vero soluta.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="carousel-item">
+                <div class="banner_dos">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-md-6 texto_banner">
+                                    <h1><span>•┃</span> NOSOTROS <span>┃•</span></h1>
+                                    <hr>
+                                    <h2> Panaderia, pasteleria y reposteria.</h2>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                                    Minima impedit a aperiam officia vel quasi eveniet doloremque maiores vero soluta.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+            </div>
 
+            <div class="carousel-item">
+                <div class="banner_tres">
+                        <div class="container float-right">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <p> </p>
+                                </div>
+                                <div class="col-md-6 texto_banner">
+                                    <h1><span>•┃</span> NOSOTROS <span>┃•</span></h1>
+                                    <hr>
+                                    <h2> Panaderia, pasteleria y reposteria.</h2>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                                    Minima impedit a aperiam officia vel quasi eveniet doloremque maiores vero soluta.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+</section>
 
+<section>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card primera">
+                    <img src="img/AboutUs3.jpg" class="card-img-top img" alt="...">
+                    <div class="card-body">
+                        <p class="card-text">
+                            Nuestra historoia.<br>
+                            Some quick example text to build on the card title and make up the bulk of the card's content.
+                            Some quick example text to build on the card title and make up the bulk of the card's content.
+                            Some quick example text to build on the card title and make up the bulk of the card's content.
+                            Some quick example text to build on the card title and make up the bulk of the card's content.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-8">
+                <div class="card mb-4 segunda" >
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src="img/AboutUs2.png" class="img-fluid rounded-start img" alt="...">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h4 class="card-title">MISIÓN</h4>
+                                <hr>
+                                <p class="card-text">
+                                    Estamos comprometidos con pasión a la elaboración de los mejores productos, 
+                                    generando una experiencia única en nuestros clientes.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>  
+
+                <div class="card mb-4 segunda">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src="img/AboutUs1.png" class="img-fluid rounded-start img" alt="...">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">VISIÓN</h5>
+                                <hr>
+                                <p class="card-text">
+                                Ser reconocidos como la mejor panadería y pastelería del El Salvador, en cuanto a
+                                productos y servicios de excelencia, calidad e innovación continua.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+</section>
+
+<section>
+    <h2>Nuestros valores</h2>
+    <hr>
+    <div class="row row-cols-1 row-cols-md-3 g-4">
+        <div class="col">
+            <div class="card h-100 valores" >
+                <img src="img/AboutUsIcon3.png" class="card-img-top img" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Calidad</h5>
+                </div>
+            </div>
+        </div>
+
+        <div class="col">
+            <div class="card h-100 valores">
+                <img src="img/AboutUsIcon2.png" class="card-img-top img" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Excelencia</h5>
+                </div>
+            </div>
+        </div>
+
+        <div class="col">
+            <div class="card h-100 valores">
+                <img src="img/AboutUsIcon4.png" class="card-img-top img" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Integridad</h5>
+                </div>
+            </div>
+        </div>
+
+        <div class="col">
+            <div class="card h-100 valores">
+                <img src="img/AboutUsIcon1.png" class="card-img-top img" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Pasión</h5>
+                </div>
+            </div>
+        </div>
+
+        <div class="col">
+            <div class="card h-100 valores">
+                <img src="img/AboutUsIcon6.png" class="card-img-top img" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Responsabilidad</h5>
+                </div>
+            </div>
+        </div>
+
+        <div class="col">
+            <div class="card h-100 valores">
+                <img src="img/AboutUsIcon5.png" class="card-img-top img" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Trabajo en equipo</h5>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</section>
 <?php include("./TEMPLATE/pie.php") ?>
